6c791b95581123db294c30fe11303423f890eac5
Peter00010 <mwongela.oboe@gmail.com>
2024-02-04 09:42:06 +0300

    Removed unnecessary text

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import React, { useState } from 'react';
-// import TransactionList from './components/TransactionList';
-// import AddTransactionForm from './components/AddTransactionForm';
-// import './App.css'
-
-//     const App = () => {
-//     const [searchTerm, setSearchTerm] = useState('');
-//     const [transactions, setTransactions] = useState([]);
-
-//     const handleAddTransaction = (newTransaction) =>{
-//         setTransactions([...transactions, newTransaction]);
-//     };
-
-//     return (
-//         <div>
-//         <h1 className='heading'>Bank  Of FlatIron</h1>
-//         <TransactionList searchTerm={searchTerm} />
-//         <AddTransactionForm onAddTransaction={handleAddTransaction} />
-//         </div>
-//     );
-//     };
-
-// export default App;
-
-
 import React, { useState } from 'react';
 import TransactionList from './components/TransactionList';
 import AddTransactionForm from './components/AddTransactionForm';
